bf979916d9ab03671ebe90de19a1c868637016e4
Justin Morris <jgm1844@g.rit.edu>
2022-09-13 16:48:56 -0400

    Added expandable scriptable object drawer package

---
+++ ExpandableScriptableObjects/README.md
@@ -0,0 +1,1 @@
+Allows ScriptableObjects with the "Expandable" attribute to be expanded and edited in the editor.
---
+++ ExpandableScriptableObjects/Runtime/ExpandableAttribute.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+namespace JMor
+{
+	public class ExpandableAttribute : PropertyAttribute
+	{
+	}
+}
